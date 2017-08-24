92227d0c3b85079c539581d470c89cecfcd2814b
Prima Aulia Gusta <primaulia@gmail.com>
2017-08-24 11:45:55 +0800

    latest state change with input fields and button

--- src/Counter.js
+++ src/Counter.js
@@ -4,21 +4,80 @@ class Counter extends Component {
   constructor (props) {
     super()
 
+    // only time you can initialize state with = sign
     this.state = {
-      number: props.num
+      number: props.num,
+      name: 'Typed your name',
+      namesArr: []
     }
+
+    // console.log('at constructor', this)
   }
 
   increaseCounter (e) {
-    console.log('this', this)
-    this.setState({ number: this.state.number + 1 })
+    // console.log('at increaseCounter', this)
+    this.setState({
+      number: this.state.number + 1
+    })
+  }
+
+  showName (e) {
+    // console.log(e.target.value)
+    if (e.target.value) {
+      let typedName = e.target.value
+
+      this.setState((prevState) => {
+        console.log('prev state', prevState)
+        console.log('name', typedName)
+        return {
+          name: typedName
+        }
+      })
+    } else {
+      this.setState({ name: 'Typed your name' })
+    }
+  }
+
+  updateList (e) {
+    this.setState({
+      namesArr: this.state.namesArr.concat(this.state.name)
+    })
   }
 
   render () {
+    let allNames = this.state.namesArr.map((name, index) => {
+      return <li key={index}>{name}</li>
+    })
+
     return (
       <div>
-        <h1>Counter: {this.state.number}</h1>
-        <button onClick={(e) => this.increaseCounter(e)}>boom!</button>
+        <h1>
+          {/* {{}}
+            <%%>
+            <%%>
+            - -
+          */}
+          Counter: {this.state.number}
+        </h1>
+        <h2>{this.state.name}</h2>
+        <div>
+          <label>
+            Type your name:
+            <input type='text' onChange={(e) => this.showName(e)} />
+          </label>
+        </div>
+        <button
+          onClick={(e) => this.increaseCounter(e)}>
+          boom!
+        </button>
+        <button onClick={(e) => this.updateList(e)}>
+          New
+        </button>
+
+        <h2>Arya's kill list</h2>
+        <ul>
+          {allNames}
+        </ul>
       </div>
     )
   }
