3bfe4a96506370d3b8df079437cf9ee22c55585c
Prima Aulia Gusta <primaulia@gmail.com>
2017-08-24 16:31:50 +0800

    latest counter

--- src/Counter.js
+++ src/Counter.js
@@ -14,39 +14,50 @@ class Counter extends Component {
     this.state = {
       number: props.num,
       keyword: 'Search keyword',
-      enemiesArr: [
-        'The Hound', 'Cersei', 'Sansa'
-      ]
+      enemiesArr: []
     }
 
     // console.log('at constructor', this)
   }
 
-  increaseCounter (e) {
-    // console.log('at increaseCounter', this)
-    this.setState({
-      number: this.state.number + 1
+  // increaseCounter (e) {
+  //   // console.log('at increaseCounter', this)
+  //   this.setState({
+  //     number: this.state.number + 1
+  //   })
+  // }
+  //
+  // showName (e) {
+  //   // console.log(e.target.value)
+  //   if (e.target.value) {
+  //     let typedName = e.target.value
+  //
+  //     this.setState((prevState) => {
+  //       return {
+  //         keyword: typedName
+  //       }
+  //     })
+  //   } else {
+  //     this.setState({ keyword: 'Search keyword:' })
+  //   }
+  // }
+  //
+  // updateList (e) {
+  //   this.setState({
+  //     namesArr: this.state.namesArr.concat(this.state.name)
+  //   })
+  // }
+
+  filterName (e) {
+    let keyword = e.target.value
+
+    const results = this.state.enemiesArr.filter((name) => {
+      return name.toLowerCase().includes(keyword.toLowerCase())
+      // 'the hound'.includes('t')
     })
-  }
 
-  showName (e) {
-    // console.log(e.target.value)
-    if (e.target.value) {
-      let typedName = e.target.value
-
-      this.setState((prevState) => {
-        return {
-          keyword: typedName
-        }
-      })
-    } else {
-      this.setState({ keyword: 'Search keyword:' })
-    }
-  }
-
-  updateList (e) {
     this.setState({
-      namesArr: this.state.namesArr.concat(this.state.name)
+      enemiesArr: results
     })
   }
 
@@ -57,12 +68,17 @@ class Counter extends Component {
 
     return (
       <div>
-        <DisplayedCounter number={this.state.number} />
+        <label>
+          Search
+          <input type='text' onChange={(e) => this.filterName(e)} />
+        </label>
 
-        <button
+        {/* <DisplayedCounter number={this.state.number} />
+
+          <button
           onClick={(e) => this.increaseCounter(e)}>
           +1
-        </button>
+        </button> */}
 
         <h2>Arya's kill list</h2>
         <ul>
@@ -87,6 +103,31 @@ class Counter extends Component {
 
       </div>
     )
+  }
+
+  // only run api here, on initial rendering
+  componentDidMount () {
+    const url = 'https://api.got.show/api/characters'
+
+    fetch(url)
+      .then((response) => { // promise is resolved, and response is received
+        // console.log('response', response)
+        return response.json() // convert response.body into json format
+      })
+      .then((data) => {
+        // console.log('data', data) // reads the json
+        data.map((character, index) => {
+          if (index < 101) {
+            console.log(character.name)
+            this.setState({
+              enemiesArr: this.state.enemiesArr.concat(character.name)
+            })
+          }
+        })
+      })
+      .catch((err) => {
+        console.log('err', err) // just in case if api call fails
+      })
   }
 }
 
